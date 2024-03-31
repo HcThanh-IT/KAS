3770670b0c6959767adb398b61de0aa410423df3
HcThanh-IT <thanhhoang1752003@gmail.com>
2024-03-31 23:40:02 +0700

    a

---
+++ App/KAS/KAS/Sign_in.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace KAS
+{
+    partial class Sign_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_TiepTuc = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btn_TiepTuc);
+            this.panel1.Controls.Add(this.btn_Thoat);
+            this.panel1.Controls.Add(this.btn_Xoa);
+            this.panel1.Controls.Add(this.btn_0);
+            this.panel1.Controls.Add(this.btn_9);
+            this.panel1.Controls.Add(this.btn_8);
+            this.panel1.Controls.Add(this.btn_7);
+            this.panel1.Controls.Add(this.btn_6);
+            this.panel1.Controls.Add(this.btn_5);
+            this.panel1.Controls.Add(this.btn_4);
+            this.panel1.Controls.Add(this.btn_3);
+            this.panel1.Controls.Add(this.btn_2);
+            this.panel1.Controls.Add(this.btn_1);
+            this.panel1.Location = new System.Drawing.Point(500, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_TiepTuc
+            // 
+            this.btn_TiepTuc.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_TiepTuc.AutoSize = true;
+            this.btn_TiepTuc.BackColor = System.Drawing.Color.Lime;
+            this.btn_TiepTuc.Location = new System.Drawing.Point(155, 360);
+            this.btn_TiepTuc.Name = "btn_TiepTuc";
+            this.btn_TiepTuc.Size = new System.Drawing.Size(95, 60);
+            this.btn_TiepTuc.TabIndex = 40;
+            this.btn_TiepTuc.Text = "Tiếp tục";
+            this.btn_TiepTuc.UseVisualStyleBackColor = false;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Thoat.AutoSize = true;
+            this.btn_Thoat.BackColor = System.Drawing.Color.Silver;
+            this.btn_Thoat.Location = new System.Drawing.Point(50, 360);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(95, 60);
+            this.btn_Thoat.TabIndex = 39;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = false;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Xoa.AutoSize = true;
+            this.btn_Xoa.BackColor = System.Drawing.Color.Red;
+            this.btn_Xoa.Location = new System.Drawing.Point(120, 290);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(130, 60);
+            this.btn_Xoa.TabIndex = 38;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = false;
+            // 
+            // btn_0
+            // 
+            this.btn_0.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_0.AutoSize = true;
+            this.btn_0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_0.Location = new System.Drawing.Point(50, 290);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(60, 60);
+            this.btn_0.TabIndex = 37;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            // 
+            // btn_9
+            // 
+            this.btn_9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_9.AutoSize = true;
+            this.btn_9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_9.Location = new System.Drawing.Point(190, 220);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(60, 60);
+            this.btn_9.TabIndex = 36;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            // 
+            // btn_8
+            // 
+            this.btn_8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_8.AutoSize = true;
+            this.btn_8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_8.Location = new System.Drawing.Point(120, 220);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(60, 60);
+            this.btn_8.TabIndex = 35;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            // 
+            // btn_7
+            // 
+            this.btn_7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_7.AutoSize = true;
+            this.btn_7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_7.Location = new System.Drawing.Point(50, 220);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(60, 60);
+            this.btn_7.TabIndex = 34;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            // 
+            // btn_6
+            // 
+            this.btn_6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_6.AutoSize = true;
+            this.btn_6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_6.Location = new System.Drawing.Point(190, 150);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(60, 60);
+            this.btn_6.TabIndex = 33;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_5.AutoSize = true;
+            this.btn_5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_5.Location = new System.Drawing.Point(120, 150);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(60, 60);
+            this.btn_5.TabIndex = 32;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_4.AutoSize = true;
+            this.btn_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_4.Location = new System.Drawing.Point(50, 150);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(60, 60);
+            this.btn_4.TabIndex = 31;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_3.AutoSize = true;
+            this.btn_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_3.Location = new System.Drawing.Point(190, 80);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(60, 60);
+            this.btn_3.TabIndex = 30;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_2.AutoSize = true;
+            this.btn_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_2.Location = new System.Drawing.Point(120, 80);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(60, 60);
+            this.btn_2.TabIndex = 29;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_1.AutoSize = true;
+            this.btn_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_1.Location = new System.Drawing.Point(50, 80);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(60, 60);
+            this.btn_1.TabIndex = 28;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 50);
+            this.textBox1.TabIndex = 41;
+            // 
+            // Sign_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Sign_in";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_TiepTuc;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ App/KAS/KAS/Sign_in.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KAS
+{
+    public partial class Sign_in : Form
+    {
+        public Sign_in()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
